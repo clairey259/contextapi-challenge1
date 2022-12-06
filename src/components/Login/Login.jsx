1219cd105f4d24db99692b0cdeab9abc659ccb75
Claire <claire.huizenga@outlook.com>
2022-12-06 14:48:59 +1100

    can sign in with google

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,50 +1,37 @@
 import React, { useContext } from "react";
 import { UserContext } from "../../context/UserProvider/UserProvider";
 import Styles from "./Login.module.scss";
-import { app } from "../../firebase";
-import {
-  getAuth,
-  signInWithEmailAndPassword,
-  signInWithRedirect,
-  updateCurrentUser,
-} from "firebase/auth";
-import { GoogleAuthProvider } from "firebase/auth";
+import { signInWithEmailAndPassword, updateCurrentUser } from "firebase/auth";
 import BasicForm from "../BasicForm/BasicForm";
 import { ThemeContext } from "../../context/ThemeProvider/ThemeProvider";
-import { db, auth, handleGoogleLogin} from "../../firebase";
+import { auth, handleGoogleLogin } from "../../firebase";
 
-const Login = ({navigate}) => {
+const Login = ({ navigate }) => {
   const theme = useContext(ThemeContext);
 
   const user = useContext(UserContext);
-  const provider = new GoogleAuthProvider();
 
+  const handleUpdateUser = async (userInfo) => {
+    await user.setCurrentUser(userInfo);
+  };
 
-  // const handleGoogleLogin = (e) => {
-  //   signInWithRedirect(getAuth(app), provider)
-  //     .then((res) => {
-  //       console.log(res)
-  //       updateCurrentUser(res)
-  //       user.setCurrentUser(app.User.email)
-  //     })
-  // };
+  const handleAlertandRedirect = (message, time) => {
+    alert(message);
+    setTimeout(() => {
+      navigate("/");
+    }, time);
+  };
 
   const handleLoginSubmit = (e) => {
     e.preventDefault();
     const email = e.target[0].value;
     const password = e.target[1].value;
-    
-  const handleUpdateUser = (userInfo) => {
-        user.setCurrentUser(userInfo);
-        updateCurrentUser(userInfo)
-  }
+
     signInWithEmailAndPassword(auth, email, password)
       .then((res) => {
-        handleUpdateUser(res.user.email)
-        alert("welcome back to MazinApp");
-        setTimeout(() => {
-          navigate("/");
-        }, 1000);
+        handleUpdateUser(res.user.email);
+        updateCurrentUser(res.user.email);
+        handleAlertandRedirect("Welcome back to MazinApp", 1000);
       })
 
       .catch((e) => {
@@ -53,7 +40,10 @@ const Login = ({navigate}) => {
   };
 
   return (
-    <section className={theme.isDarkTheme ? Styles.dark : Styles.light} data-testid="themeContainer">
+    <section
+      className={theme.isDarkTheme ? Styles.dark : Styles.light}
+      data-testid="themeContainer"
+    >
       <form action="action_page.php" method="post" onSubmit={handleLoginSubmit}>
         <div className={Styles.container}>
           <BasicForm
@@ -65,20 +55,26 @@ const Login = ({navigate}) => {
       </form>
       <p
         onClick={(e) => {
-          handleGoogleLogin();
-        }} data-testid="googleLogin"
+          handleGoogleLogin()
+            .then((res) => handleUpdateUser(res))
+            .then(() => handleAlertandRedirect("Welcome to MazinApp", 1000))
+            .catch(() =>
+              handleAlertandRedirect("Sorry! Something went wrong", 2000)
+            );
+        }}
+        data-testid="googleLogin"
       >
         <b>Login</b> With Your Google Account{" "}
       </p>
       <p
         onClick={(e) => {
           navigate("/CreateAccount");
-        }} data-testid="createAccount"
+        }}
+        data-testid="createAccount"
       >
         Don't have an account? <b>Create</b> an Account
       </p>
     </section>
-    
   );
 };
 
--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,23 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import {
-  GoogleAuthProvider, 
+  GoogleAuthProvider,
   getAuth,
   signInWithPopup,
   updateCurrentUser,
 } from "firebase/auth";
+import {
+  getFirestore,
+  query,
+  getDocs,
+  collection,
+  where,
+  doc,
+  addDoc,
+  setDoc,
+} from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-import { getFirestore } from "firebase/firestore"
-import { UserContext } from "./context/UserProvider/UserProvider";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -18,7 +26,7 @@ const firebaseConfig = {
   projectId: "mazinapp-a38c5",
   storageBucket: "mazinapp-a38c5.appspot.com",
   messagingSenderId: "996377006559",
-  appId: "1:996377006559:web:e91fd7dcf0ed224e45346a"
+  appId: "1:996377006559:web:e91fd7dcf0ed224e45346a",
 };
 
 // Initialize Firebase
@@ -26,28 +34,24 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 const googleProvider = new GoogleAuthProvider();
-const currentUser = UserContext(UserContext);
 
+const googleUserRef = collection(db, "users");
 
 const handleGoogleLogin = async (e) => {
-  const response = await signInWithPopup(auth, googleProvider)
-  const googleUser = response.user
-    .then(() => {
-      updateCurrentUser(googleUser.email);
-      currentUser.setCurrentUser(googleUser.email);
-      
-      alert("welcome back to MazinApp");
-        setTimeout(() => {
-          navigate("/");
-        }, 1000);
-    })
-  console.log(response)
-  console.log(googleUser)
-    // .then((res) => {
-    //   updateCurrentUser(res)
-    //   user.setCurrentUser(app.User.email)
-    // })
+  const response = await signInWithPopup(auth, googleProvider);
+  const googleUser = response.user;
+  const q = query(googleUserRef, where("email", "==", googleUser.email));
+  const qDoc = await getDocs(q)
+
+  if (qDoc.empty) {
+    await setDoc(doc(googleUserRef, googleUser.displayName), {
+      email: googleUser.email
+    });
+    return googleUser.email;
+  } else {
+    return googleUser.email
+  }
+
 };
-export {
-  auth, db, handleGoogleLogin
-}
+
+export { auth, db, handleGoogleLogin };
